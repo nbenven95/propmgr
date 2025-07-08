5e36a48140d8193313058b68331d85398123bc86
Noah Benveniste <nbenven@RISEIT4.TSA.local>
2025-07-08 15:42:41 -0400

    Initialized frontend React project with Vite + ChakraUI frameworks; tested for basic functionality.

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+const rootElement = document.getElementById('root');
+createRoot(rootElement).render(
+  <StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </StrictMode>,
+);
