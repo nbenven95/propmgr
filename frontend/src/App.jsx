4268dea54132b74001ea2d612ff044820fd03c35
Noah Benveniste <nbenven@RISEIT4.TSA.local>
2025-07-09 13:32:27 -0400

    Implemented initial FilesPage and UploadPage views. Need to finish initial backend implementation to finish testing file upload/delete functionality; from then on, all edits to backend/ should be done on the backend branch.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import FilesPage from './pages/FilesPage'
+import UploadPage from './pages/UploadPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <nav style={{ padding: '10px', gap: '10px' }}>
+        <Link to='/upload'>Upload files</Link> | <Link to='/files'>View uploaded files</Link>
+      </nav>
+      <Routes>
+        <Route path='/upload' element={<UploadPage />} />
+        <Route path='/files' element={<FilesPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
 const rootElement = document.getElementById('root');
